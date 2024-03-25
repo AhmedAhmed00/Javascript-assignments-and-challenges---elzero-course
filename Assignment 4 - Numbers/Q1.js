991e86e1604e19a6bdb6631027a02b8665b42442
AhmedAhmed00 <devahmedahmed@gmail.com>
2024-03-25 11:05:34 +0200

    assignment4

---
+++ Assignment 4 - Numbers/Q1.js
@@ -0,0 +1,16 @@
+// Examples
+console.log(100_000); // 100000
+console.log(100000); // 100000
+console.log(5e4 + 5e4); // 100000
+
+// Your Solutions
+console.log(); // 100000
+console.log(); // 100000
+console.log(); // 100000
+console.log(); // 100000
+console.log(); // 100000
+console.log(); // 100000
+console.log(); // 100000
+console.log(); // 100000
+console.log(); // 100000
+console.log(); // 100000
