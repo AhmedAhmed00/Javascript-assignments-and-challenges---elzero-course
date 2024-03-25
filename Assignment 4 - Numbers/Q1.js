772caa90d814bff194be5e2ddead851c566a688b
AhmedAhmed00 <devahmedahmed@gmail.com>
2024-03-25 11:57:53 +0200

    assignment 4

--- Assignment 4 - Numbers/Q1.js
+++ Assignment 4 - Numbers/Q1.js
@@ -4,13 +4,13 @@ console.log(100000); // 100000
 console.log(5e4 + 5e4); // 100000
 
 // Your Solutions
-console.log(); // 100000
-console.log(); // 100000
-console.log(); // 100000
-console.log(); // 100000
-console.log(); // 100000
-console.log(); // 100000
-console.log(); // 100000
-console.log(); // 100000
-console.log(); // 100000
-console.log(); // 100000
+console.log(Math.pow(10, 5)); // 100000
+console.log(10 ** 5); // 100000
+console.log(1e5); // 100000
+console.log(100000.0000); // 100000
+console.log((100000.23).toFixed()); // 100000
+console.log(Math.round(100000.23)); // 100000
+console.log(parseInt("100000")); // 100000
+console.log(parseFloat(100000)); // 100000
+console.log(Math.floor(100000.232)); // 100000
+console.log(Math.ceil(99999.2332)); // 100000
